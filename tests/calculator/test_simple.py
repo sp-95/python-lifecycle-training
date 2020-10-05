698d38b791cedd258c45c37c90cd46ab0cb1447d
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-06 01:32:17 +0545

    chore: Add isort

--- tests/calculator/test_simple.py
+++ tests/calculator/test_simple.py
@@ -1,9 +1,11 @@
-from python_lifecycle_training import __version__
-import python_lifecycle
-from pytest import fixture
-import pytest
 import os
 from pathlib import Path
+
+import pytest
+import python_lifecycle
+from pytest import fixture
+
+from python_lifecycle_training import __version__
 
 
 def test_version():
