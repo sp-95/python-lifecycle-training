8efe65a0e7c113a86a4f73fbe3e5f7a431f133a8
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-06 19:59:43 +0545

    chore: Add logs for complex calculator

--- python_lifecycle_training/calculator/complex.py
+++ python_lifecycle_training/calculator/complex.py
@@ -1,21 +1,26 @@
 import fire
+from loguru import logger
 
 
 class Calculator:
     @staticmethod
     def add(a, b):
+        logger.info(f"Adding {a} to {b}")
         return a + b
 
     @staticmethod
     def sub(a, b):
+        logger.info(f"Subtracting {b} from {a}")
         return a - b
 
     @staticmethod
     def mul(a, b):
+        logger.info(f"Multiplying {a} by {b}")
         return a * b
 
     @staticmethod
     def div(a, b):
+        logger.info(f"Dividing {a} by {b}")
         return a / b
 
 
