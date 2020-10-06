e819bfbfe9bfda0eb336df500dda90b8cdfe0046
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-06 17:29:55 +0545

    feat: Create a complex calculator and add CLI using python fire

---
+++ python_lifecycle_training/calculator/complex.py
@@ -0,0 +1,23 @@
+import fire
+
+
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    @staticmethod
+    def div(a, b):
+        return a / b
+
+
+def main():
+    fire.Fire(Calculator)
--- python_lifecycle_training/calculator/simple.py
+++ python_lifecycle_training/calculator/simple.py
@@ -1,5 +1,11 @@
 from numbers import Real
 
+import fire
+
 
 def add(a: Real, b: Real) -> Real:
     return a + b
+
+
+def main():
+    fire.Fire(add)
