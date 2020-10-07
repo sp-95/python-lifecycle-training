515cf93647f35b6de9651f4d276a1406ff8ceae3
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-07 13:13:48 +0545

    test: Single test with multiple params for simple calculator

--- tests/calculator/test_simple.py
+++ tests/calculator/test_simple.py
@@ -3,21 +3,24 @@ import pytest
 from python_lifecycle_training.calculator import simple
 
 
-def test_add_positives():
-    assert simple.add(1, 2) == 3
+def isfloat(num):
+    return isinstance(num, float)
 
 
-def test_add_negatives():
-    assert simple.add(-1, -2) == -3
+@pytest.mark.parametrize(
+    "a, b",
+    [
+        (1, 2),
+        (-1, -2),
+        (1, -2),
+        (0.1, 0.2),
+        ("hello ", "world"),
+    ],
+)
+def test_add(a, b):
+    expected = a + b
 
+    if any(map(isfloat, (a, b))):
+        expected = pytest.approx(expected)
 
-def test_add_mixed():
-    assert simple.add(1, -2) == -1
-
-
-def test_add_float():
-    assert simple.add(0.1, 0.2) == pytest.approx(0.3)
-
-
-def test_add_str():
-    assert simple.add("hello ", "world") == "hello world"
+    assert simple.add(a, b) == expected
