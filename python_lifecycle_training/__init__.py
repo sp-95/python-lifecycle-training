dcca70b381f9463bb8434f27bb1c63e7a306ac7b
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-07 09:14:19 +0545

    fix: Add back __version__

--- python_lifecycle_training/__init__.py
+++ python_lifecycle_training/__init__.py
@@ -5,3 +5,5 @@ logger.add("logs/error.log", level="ERROR", rotation="10MB")
 logger.add("logs/warning.log", level="WARNING", rotation="10MB")
 logger.add("logs/info.log", level="INFO", rotation="10MB")
 logger.add("logs/debug.log", level="DEBUG", rotation="10MB")
+
+__version__ = "0.1.0"
