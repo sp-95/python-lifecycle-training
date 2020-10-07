5dfbaca09b63cfd04d742aa763ed92d340ee8127
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-07 21:41:19 +0545

    refactor: Change everything to classmethod in broken calculator

--- python_lifecycle_training/calculator/broken.py
+++ python_lifecycle_training/calculator/broken.py
@@ -7,24 +7,26 @@ from python_lifecycle_training.calculator.complex import Calculator
 
 
 class BrokenCalculator(Calculator):
-    def __init__(self):
-        with settings.using_env(python_lifecycle_training.ENV):
-            offset = settings.offset
-            logger.info(f"Offset: {offset}")
-            logger.info(f"Set by: {settings.name}")
-            self.offset = offset
+    with settings.using_env(python_lifecycle_training.ENV):
+        offset = settings.offset
+        logger.info(f"Offset: {offset}")
+        logger.info(f"Set by: {settings.name}")
 
-    def add(self, a, b):
-        return super(BrokenCalculator, self).add(a, b) - self.offset
+    @classmethod
+    def add(cls, a, b):
+        return super(BrokenCalculator, cls).add(a, b) - cls.offset
 
-    def sub(self, a, b):
-        return super(BrokenCalculator, self).sub(a, b) - self.offset
+    @classmethod
+    def sub(cls, a, b):
+        return super(BrokenCalculator, cls).sub(a, b) - cls.offset
 
-    def mul(self, a, b):
-        return super(BrokenCalculator, self).mul(a, b) - self.offset
+    @classmethod
+    def mul(cls, a, b):
+        return super(BrokenCalculator, cls).mul(a, b) - cls.offset
 
-    def div(self, a, b):
-        return super(BrokenCalculator, self).div(a, b) - self.offset
+    @classmethod
+    def div(cls, a, b):
+        return super(BrokenCalculator, cls).div(a, b) - cls.offset
 
 
 def main():
