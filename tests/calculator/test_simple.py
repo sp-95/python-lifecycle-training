44078cdb6b07fd6078c7331f3eafd0c38026d788
Shashanka Prajapati <shashanka@fusemachines.com>
2020-10-07 11:03:32 +0545

    test: Simple addition tests

--- tests/calculator/test_simple.py
+++ tests/calculator/test_simple.py
@@ -1,5 +1,23 @@
-from python_lifecycle_training import __version__
+import pytest
+
+from python_lifecycle_training.calculator import simple
 
 
-def test_version() -> None:
-    assert __version__ == "0.1.0"
+def test_add_positives():
+    assert simple.add(1, 2) == 3
+
+
+def test_add_negatives():
+    assert simple.add(-1, -2) == -3
+
+
+def test_add_mixed():
+    assert simple.add(1, -2) == -1
+
+
+def test_add_float():
+    assert simple.add(0.1, 0.2) == pytest.approx(0.3)
+
+
+def test_add_str():
+    assert simple.add("hello ", "world") == "hello world"
